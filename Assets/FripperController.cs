a285b85dffe818905510a1fe99b61c43c00ccee6
rakai <ra9g16@soton.ac.uk>
2017-07-02 16:02:41 +0200

    PinBallプロジェクト 最初のコミット
    課題と発展課題を行いました。ご確認ください。

---
+++ Assets/FripperController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FripperController : MonoBehaviour {
+	//HingiJointコンポーネントを入れる
+	private HingeJoint myHingeJoint;
+
+	//初期の傾き
+	private float defaultAngle = 20;
+	//弾いた時の傾き
+	private float flickAngle = -20;
+
+	// Use this for initialization
+	void Start () {
+		//HinjiJointコンポーネント取得
+		this.myHingeJoint = GetComponent<HingeJoint>();
+
+		//フリッパーの傾きを設定
+		SetAngle(this.defaultAngle);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//左矢印キーを押した時左フリッパーを動かす
+		if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+		//右矢印キーを押した時右フリッパーを動かす
+		if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.flickAngle);
+		}
+
+		//矢印キー離された時フリッパーを元に戻す
+		if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+		if (Input.GetKeyUp (KeyCode.RightArrow) && tag == "RightFripperTag") {
+			SetAngle (this.defaultAngle);
+		}
+
+		if (Input.touchCount > 0) {
+			Touch touch0 = Input.GetTouch (0);
+			if (touch0.phase == TouchPhase.Began) {
+				if (touch0.position.x < Screen.width / 2 && tag == "LeftFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+				if (touch0.position.x > Screen.width / 2 && tag == "RightFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+			} else if (touch0.phase == TouchPhase.Ended) {
+				if (touch0.position.x < Screen.width / 2 && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+				if (touch0.position.x > Screen.width / 2 && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+		}
+
+		if (Input.touchCount > 0) {
+			Touch touch1 = Input.GetTouch (1);
+			if (touch1.phase == TouchPhase.Began) {
+				if (touch1.position.x < Screen.width/2 && tag == "LeftFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+				if (touch1.position.x > Screen.width/2 && tag == "RightFripperTag") {
+					SetAngle (this.flickAngle);
+				}
+			} else if (touch1.phase == TouchPhase.Ended) {
+				if (touch1.position.x < Screen.width/2 && tag == "LeftFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+				if (touch1.position.x > Screen.width/2 && tag == "RightFripperTag") {
+					SetAngle (this.defaultAngle);
+				}
+			}
+		}
+	}
+
+	//フリッパーの傾きを設定
+	public void SetAngle (float angle){
+		JointSpring jointSpr = this.myHingeJoint.spring;
+		jointSpr.targetPosition = angle;
+		this.myHingeJoint.spring = jointSpr;
+	}
+}
---
+++ Assets/PointController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PointController : MonoBehaviour {
+
+	//得点を表示するテキスト
+	private GameObject pointText;
+
+	//特典の変数
+	private int point = 0;
+
+	void OnCollisionEnter (Collision Col) {
+		if (Col.gameObject.tag == "SmallStarTag") {
+			point += 10;
+		} else if (Col.gameObject.tag == "LargeStarTag") {
+			point += 20;
+		} else if (Col.gameObject.tag == "SmallCloudTag") {
+			point += 30;
+		} else if (Col.gameObject.tag == "LargeCloudTag") {
+			point += 40;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		//シーン中のPointTextオブジェクトを取得
+		this.pointText = GameObject.Find("PointText");	
+		this.pointText.GetComponent<Text> ().text = "Score: 0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.pointText.GetComponent<Text> ().text = "Score: "+point;
+	}
+}
